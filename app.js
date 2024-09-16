8658cb9a58e85b1455b0b3380c13129dfe01782d
Mandylaaane <ljungamanda@gmail.com>
2024-09-16 12:50:07 +0200

    4th draft, JS done. Adding CSS.

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-//global variables
+//GLOBAL VARIABLES
 let color = "black";
 
+//16x16 GRID
 
-
-//Create a webpage with a 16x16 grid of square divs.
 function populateBoard(size) {
     let board = document.querySelector('.board');
     let squares = board.querySelectorAll('div');
@@ -22,7 +21,7 @@ function populateBoard(size) {
 
 populateBoard(16);
 
-//Set up a “hover” effect so that colors change when mouse hover. (line: 12)
+
 function changeSize(input) {
     if (input >=2 && input <=100 ) {
         populateBoard(input);
@@ -31,6 +30,8 @@ function changeSize(input) {
         console.log("too many squares");
     }
 }
+
+//CHANGE COLOR SQUARES
 
 function colorSquare() {
     if(color === 'random'){
@@ -44,10 +45,11 @@ function changeColor(choice) {
     color = choice;
 }
 
+//RESET BOARD
+
 function resetBoard() {
     let board = document.querySelector('.board');
     let squares = board.querySelectorAll('div');
     squares.forEach((div) => div.remove());
 }
 
-//Add a button on the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. 
